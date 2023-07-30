496062d96be56088ab5dc05abb3d5729a525bbaf
Daibel Inle <konradzuse.dm@gmail.com>
2023-07-30 00:16:32 -0400

    linux-app-installer script

---
+++ linux-installation-app.sh
@@ -0,0 +1,8 @@
+#  Author: Daibel Inle Martínez Sáchez [Konrad]
+#  Date  : 2023-07-30 00:10
+#  This is a script squeleeton to verify system distribution [GNU/Linux]
+#  and package manager and execute a bash function to perform app/package installation
+#  from  oficial repository private hosting service or downloaded package.
+#  Uding this you are accepting the disclaimer on this github project README.
+
+
